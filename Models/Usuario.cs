9fc78c0cfb26c6fcf0fedbc7e8b1ff7e482855ab
Yuri Muniz Maldonado <yuri.maldonado@castgroup.com.br>
2021-11-04 10:20:23 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TelefericoWebApp.Models
+{
+    public class Usuario
+    {
+        public int UsuarioId { get; set; }
+        public string Nome { get; set; }
+        public string Login { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string Endereço { get; set; }
+        public decimal Cartao { get; set; }
+        public bool Status { get; set; }
+
+    }
+}
